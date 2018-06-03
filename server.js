7aa93a546834cc83e212e68e72798b01c4c118e1
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-03 17:03:20 -0400

    Adding basic controller endpoint to test heroku deployment

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var exphbs = require("express-handlebars");
+var mongoose = require('mongoose');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use('/public', express.static(__dirname + "/public"))
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('./controllers/html_endpoints.js')(app);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
