15061086a2f53ec141edaa3c1e18d6691da25caa
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-04 17:01:01 -0400

    Working towards getting basic page layout functional.  Switched from bootstrap v3.* to >v4.  Page currently is behaving responsively.

---
+++ public/assets/scripts/scraper.js
@@ -0,0 +1,11 @@
+var scraper = (function ($){
+  'use strict';
+
+  $('#articlesNavLink').click(function () {
+    console.log('Articles nav item was clicked.');
+  });
+
+  $('#homeNavLink').click(function () {
+    console.log('Articles nav item was clicked.');
+  });
+})($);
