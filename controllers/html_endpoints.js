37b4d952a02c4e289600f1568a38ea369f45b5ff
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-04 17:35:06 -0400

    Set up navigation between the index and articles route via the site navbar.

--- controllers/html_endpoints.js
+++ controllers/html_endpoints.js
@@ -1,4 +1,8 @@
 module.exports = function (app) {
+  app.get('/articles', (req, res) => {
+    res.render('articles', {});
+  });
+  
   app.get('*', function (req, res) {
     res.render('index', {});
   });
