4cc58d63a190191abfc300b62aeda652e1b3fd81
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-04 18:01:19 -0400

    Added frontend scripts to articles and index pages.  On load these scripts are logging a message to the console.

---
+++ public/assets/scripts/articles-page.js
@@ -0,0 +1,11 @@
+(function ($) {
+  'use strict';
+
+  function onInit() {
+    console.log('articles page front end script is loaded');
+  }
+
+  $(document).ready(function () {
+    onInit();
+  });
+})($);
---
+++ public/assets/scripts/index-page.js
@@ -0,0 +1,11 @@
+(function ($) {
+  'use strict';
+  
+  function onInit() {
+    console.log('index page front end script is loaded');
+  }
+
+  $(document).ready(function () {
+    onInit();
+  });
+})($);
