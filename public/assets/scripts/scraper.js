5ed553d46e4f63afd2dd370a057a50041e6db442
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-06 23:54:09 -0400

    Articles are now displayed when user clicks the scrape articles button.

--- public/assets/scripts/scraper.js
+++ public/assets/scripts/scraper.js
@@ -1,6 +1,8 @@
 var scraper = (function ($){
   'use strict';
 
+  let $articlesWrapper = $('#articlesWrapper');
+
   $('#articlesNavLink').click(function () {
     console.log('Articles nav item was clicked.');
   });
@@ -10,8 +12,37 @@ var scraper = (function ($){
   });
 
   $('#scrapeButton').click(function () {
-    $.get('/api/articles', function (data) {
-      console.log(data);
+    // Built by LucyBot. www.lucybot.com
+    let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "e8d7b2a2149143378f5d20b5f78bf0ee"
+    });
+    
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      for (let i = 0; i < result.response.docs.length; i++) {
+        let currDoc = result.response.docs[i];
+        attachArticle(currDoc);    
+      }
+    }).fail(function(err) {
+      throw err;
     });
   });
+
+  let attachArticle = function (article) {
+    let articleHtml = `
+      <div class="row article">
+        <div class="col-10">
+          <h3 class="abstractHdr">${article.headline.main}</h3>
+        </div>
+        <div class="col-2">
+          <button class="saveArticleBtn btn btn-info">Save Article</button>
+        </div>
+      </div>
+    `;
+    
+    $articlesWrapper.append(articleHtml);
+  };  
 })($);
